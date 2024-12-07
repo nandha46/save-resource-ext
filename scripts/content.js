359fe2518b4166e10921c7d045c15e68a0341cd6
Nandha <nandha.kumar790@gmail.com>
2024-12-07 13:44:22 +0530

    content.js added

--- scripts/content.js
+++ scripts/content.js
@@ -1,22 +1,31 @@
-const article = document.querySelector("article");
+const images = document.querySelectorAll("img");
 
-// `document.querySelector` may return null if the selector doesn't match anything.
-if (article) {
-  const text = article.textContent;
-  const wordMatchRegExp = /[^\s]+/g; // Regular expression
-  const words = text.matchAll(wordMatchRegExp);
-  // matchAll returns an iterator, convert to array to get word count
-  const wordCount = [...words].length;
-  const readingTime = Math.round(wordCount / 200);
-  const badge = document.createElement("p");
-  // Use the same styling as the publish information in an article's header
-  badge.classList.add("color-secondary-text", "type--caption");
-  badge.textContent = `⏱️ ${readingTime} min read`;
-
-  // Support for API reference docs
-  const heading = article.querySelector("h1");
-  // Support for article docs with date
-  const date = article.querySelector("time")?.parentNode;
-
-  (date ?? heading).insertAdjacentElement("afterend", badge);
+if (images) {
+  for (image of images){
+    if (image.width < 720){
+      console.log(image.width);
+      continue;
+    }  
+// create a new container to put the image and set style properties
+  const container = document.createElement("div");
+  container.style.position = "relative";
+  container.style.display = "inline-block";
+  
+  // Create a new download icon and set styles
+  const downloadIcon = document.createElement("span");
+  downloadIcon.innerHTML = "👍";
+  const anchor = document.createElement('a');
+  anchor.href = image.src;
+  anchor.setAttribute('download', 'img.jpg');
+  anchor.appendChild(downloadIcon);
+  downloadIcon.style.position = "absolute";
+  downloadIcon.style.top = "-24px";
+  downloadIcon.style.right = "-24px";
+  downloadIcon.style.fontSize = "20px";
+  
+    // downloadIcon.href = image.src;
+    image.parentNode.insertBefore(container, image);
+    container.appendChild(image);
+    container.appendChild(anchor);
+  }
 }
